9f76368ef40726a2015c478c05b4de6a2e65c5db
Thomas Dimnet <thomas.dimnet@gmail.com>
2026-03-12 16:46:30 +0100

    (Feat) Remove discord logic (#118)
    * Remove discord stuff from the CLI
    
    * Delete related files to discord
    
    * Remove discord.py (but I think I messed up something)

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import click
 
-from nx_ai.discord_service.discord_cli import discord_group
 from nx_ai.github_service.github_cli import github_group
 from nx_ai.openai_service.openai_cli import openai_group
 from nx_ai.turso_service.turso_cli import turso_group
@@ -13,7 +12,6 @@ def cli():
     pass
 
 
-cli.add_command(discord_group, name="discord")
 cli.add_command(github_group, name="github")
 cli.add_command(openai_group, name="openai")
 cli.add_command(turso_group, name="turso")
